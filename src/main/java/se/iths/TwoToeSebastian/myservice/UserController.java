832d71ed86238dc3195602f5b6fa1c7624279a49
sebas <sebastianwaltilla@hotmail.com>
2020-02-26 14:30:55 +0100

    started spring project for microservice

---
+++ src/main/java/se/iths/TwoToeSebastian/myservice/UserController.java
@@ -0,0 +1,13 @@
+package se.iths.TwoToeSebastian.myservice;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+
+
+
+
+
+}
---
+++ src/test/java/se/iths/TwoToeSebastian/myservice/MyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.TwoToeSebastian.myservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
